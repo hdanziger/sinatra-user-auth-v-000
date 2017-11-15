e82f9134d7a2430d30a92a63bfa1657288396170
Hannah Danziger <hdanziger8@gmail.com>
2017-11-15 19:19:24 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,6 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    puts params
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.name = params[:name]
     @user.email = params[:email]
